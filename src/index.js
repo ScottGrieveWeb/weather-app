6caf52738fb3f0c19f1c7cceb8a6e8f06be20899
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-01 22:16:12 +0000

    Converting temp to celsius using toCelsius func

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import "./style.css"
+import { toCelcius } from "./farenheit-to-celsius";
 
 async function searchWeather(input){
     const search = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input}?key=E5FCPUDSUFM7VMLG3AMJWYX48`);
     const searchData = await search.json();
 
     console.log(searchData);
-    console.log(`It's going to be ${searchData.days[1].temp} degrees tomorrow!`);
+    let tempF = searchData.days[1].temp;
+    let tempC = toCelcius(tempF);
+
+    console.log(`It's going to be ${tempC} degrees tomorrow!`);
 }
 
 searchWeather('glasgow');
