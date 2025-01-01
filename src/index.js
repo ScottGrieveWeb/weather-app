be3b41c387adcac1fbbab7843b55f7be0dc4faa0
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-01 21:58:32 +0000

    Initial API function test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css"
+
+async function searchWeather(input){
+    const search = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input}?key=E5FCPUDSUFM7VMLG3AMJWYX48`);
+    const searchData = await search.json();
+
+    console.log(searchData);
+    console.log(`It's going to be ${searchData.days[1].temp} degrees tomorrow!`);
+}
+
+searchWeather('glasgow');
