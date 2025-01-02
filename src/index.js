e5758df05078c321d346e3b0cc0b82ca1dda20b5
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-02 19:45:10 +0000

    Began work on processData function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import "./style.css"
 import { toCelcius } from "./farenheit-to-celsius";
 
-async function searchWeather(input){
+async function fetchData(input){
     const search = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input}?key=E5FCPUDSUFM7VMLG3AMJWYX48`);
     const searchData = await search.json();
 
-    console.log(searchData);
-    let tempF = searchData.days[1].temp;
-    let tempC = toCelcius(tempF);
-
-    console.log(`It's going to be ${tempC} degrees tomorrow!`);
+    let currentDay = {
+        conditions: searchData.currentConditions.conditions,
+        humidity: searchData.currentConditions.humidity,
+        feelsLike: searchData.currentConditions.feelslike,
+        temp: searchData.currentConditions.temp,
+        precipProb: searchData.currentConditions.precipprob,
+        snow: searchData.currentConditions.snow,
+        windSpeed: searchData.currentConditions.windspeed
+    }
+    return searchData;
 }
 
-searchWeather('glasgow');
+
+
+
+async function processData(data){
+    let searchData = await fetchData(data);
+    
+    let currentDay = {
+        date: searchData.days[0].datetime,
+        conditions: searchData.days[0].conditions,
+        feelsLike: searchData.days[0].feelslike,
+        precipProb: searchData.days[0].precipprob,
+        snow: searchData.days[0].snow,
+        sunrise: searchData.days[0].sunrise,
+        sunset: searchData.days[0].sunset,
+        temp: toCelcius(searchData.days[0].temp),
+        tempmin: toCelcius(searchData.days[0].tempmin),
+        tempmap: toCelcius(searchData.days[0].tempmax),
+        windspeed: searchData.days[0].windspeed
+    }
+
+    console.log(currentDay);
+}
+
+processData('glasgow');
