65a91db8e8a410a5a8e6b2b66f949f6aa358aa05
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-03 22:17:55 +0000

    WIP displayResults function
    Added functionality to pull results object from
    localstorage and then empty this from localstorage.
    This function is called on page load

--- src/result.js
+++ src/result.js
@@ -1,1 +1,12 @@
 import "./style.css"
+
+const resultsDiv = document.querySelector('#results');
+
+function displayResult(){
+    let userResults = JSON.parse(localStorage.getItem("result"));
+    localStorage.removeItem("result");
+    console.log(userResults);
+
+}
+
+ window.onload = displayResult();
