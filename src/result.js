265cf71d3d446e2e94895a6ca283b1ac089e4a38
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-03 23:03:10 +0000

    Amended font size and alignment of result location

--- src/result.js
+++ src/result.js
@@ -6,11 +6,11 @@ function displayResult(){
     let userResults = JSON.parse(localStorage.getItem("result"));
     localStorage.removeItem("result");
     console.log(userResults);
-
+    
     let resultLocation = document.createElement("h1");
+    resultLocation.setAttribute("id", "location-name");
     let resultLocationText = document.createTextNode(userResults.currentDay.location);
     resultLocation.appendChild(resultLocationText);
-
     resultsDiv.appendChild(resultLocation);
 }
 
