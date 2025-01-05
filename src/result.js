ae880d58218c051875d91ce30329093b8acfcbfe
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-05 12:43:49 +0000

    results display weather icons from API result info
    The results page now displays relevant weather icons
    depending on the data from the API call.
    
    The displayResults func calls the iconChecker func to
    match data to a variable with the correct icon stored,
    and this is then added into the img src on the page

--- src/result.js
+++ src/result.js
@@ -1,14 +1,102 @@
-import "./style.css"
+import "./style.css";
+//TODO explore a better method for importing icon library
+import clearDay from "./icons/clear-day.svg";
+import clearNight from"./icons/clear-night.svg";
+import cloudy from "./icons/cloudy.svg";
+import cloudDayRain from"./icons/partly-cloudy-day-rain.svg";
+import cloudyDay from "./icons/partly-cloudy-day.svg";
+import cloudyNightRain from "./icons/partly-cloudy-night-rain.svg";
+import partlyCloudyNight from "./icons/partly-cloudy-night.svg";
+import rain from "./icons/rain.svg";
+import showersDay from "./icons/showers-day.svg";
+import showersNight from "./icons/showers-night.svg";
+import sleet from"./icons/sleet.svg";
+import snowShowersDay from "./icons/snow-showers-day.svg";
+import snowShowersNight from"./icons/snow-showers-night.svg";
+import snow from "./icons/snow.svg";
+import sunset from "./icons/sunset.svg";
+import thunderRain from "./icons/thunder-rain.svg";
+import thunderShowersDay from "./icons/thunder-showers-day.svg";
+import thunderShowersNight from "./icons/thunder-showers-night.svg";
+import wind from"./icons/wind.svg";
 
 const resultsDiv = document.querySelector('#results');
+const currentIcon = document.getElementById('current-icon');
+const nextIcon = document.querySelector('#next-icon');
 
 function displayResult(){
     let userResults = JSON.parse(localStorage.getItem("result"));
     localStorage.removeItem("result");
     console.log(userResults);
     
+    
     let resultLocation = document.querySelector('#location-result');
     resultLocation.innerHTML = userResults.currentDay.location;
+    
+    let currentIconSrc = iconChecker(userResults.currentDay.icon);
+    currentIcon.src = currentIconSrc;
+    let nextIconSrc = iconChecker(userResults.nextDay.icon);
+    nextIcon.src = nextIconSrc;
 }
 
  window.onload = displayResult();
+
+ function iconChecker(input){
+    switch (input){
+        case 'clear-day':
+            return clearDay;
+            break;
+        case 'clear-night':
+            return clearNight;
+            break;
+        case 'cloudy':
+            return cloudy;
+            break;
+        case 'partly-cloudy-rain-day':
+            return cloudyDayRain;
+            break;
+        case 'partly-cloudy-day':
+            return cloudyDay;
+            break;
+        case 'partly-cloudy-night-rain':
+            return cloudyNightRain;
+            break;
+        case 'partly-cloudy-night':
+            return partlyCloudyNight;
+            break;
+        case 'rain':
+            return rain;
+            break;
+        case 'showers-day':
+            return showersDay;
+            break;
+        case 'showers-night':
+            return showersNight;
+            break;
+        case 'sleet':
+            return sleet;
+            break;
+        case 'snow-showers-day':
+            return snowShowersDay;
+            break;
+        case 'snow-showers-night':
+            return snowShowersNight;
+            break;
+        case 'snow':
+            return snow;
+            break;
+        case 'thunder-rain':
+            return thunderRain;
+            break;
+        case 'thunder-showers-day':
+            return thunderShowersDay;
+            break;
+        case 'thunder-showers-night':
+            return thunderShowersNight;
+            break;
+        case 'wind':
+            return wind;
+            break;
+    }
+ }
+
