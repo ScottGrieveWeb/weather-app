7cbbfeb43a32ab1c17be3407be1d3ba83ba8a3e7
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-10 22:51:32 +0000

    Updated dom module with the new divs

--- src/dom.js
+++ src/dom.js
@@ -1,15 +1,58 @@
 export const domItems =  {
     resultsDiv: document.querySelector('#results'),
     resultLocation: document.querySelector('#location-result'),
-    currentIcon: document.getElementById('current-icon'),
-    nextIcon: document.querySelector('#next-icon'),
-    currentTemp: document.querySelector('#temp'),
-    currentFeelslike: document.querySelector('#feelslike'),
-    nextTemp: document.querySelector('#next-temp'),
-    nextFeelslike: document.querySelector('#next-feelslike'),
-    currentCondition: document.querySelector('#condition'),
-    nextConditon: document.querySelector('#next-condition')
+    current: {
+        icon: document.getElementById('current-icon'),
+        temp: document.querySelector('#temp'),
+        feelsLike: document.querySelector('#feelslike'),
+        condition:document.querySelector('#condition')
+    },
+    1:{
+        icon: document.getElementById('1-icon'),
+        temp: document.getElementById('1-temp'),
+        feelsLike: document.getElementById('1-feelslike'),
+        condition:document.getElementById('1-condition')
+    },
+    2:{
+        icon: document.getElementById('2-icon'),
+        temp: document.getElementById('2-temp'),
+        feelsLike: document.getElementById('2-feelslike'),
+        condition:document.getElementById('2-condition')
+    },
+    3:{
+        icon: document.getElementById('3-icon'),
+        temp: document.getElementById('3-temp'),
+        feelsLike: document.getElementById('3-feelslike'),
+        condition:document.getElementById('3-condition')
+    },
+    4:{
+        icon: document.getElementById('4-icon'),
+        temp: document.getElementById('4-temp'),
+        feelsLike: document.getElementById('4-feelslike'),
+        condition:document.getElementById('4-condition')
+    },
+    5:{
+        icon: document.getElementById('5-icon'),
+        temp: document.getElementById('5-temp'),
+        feelsLike: document.getElementById('5-feelslike'),
+        condition:document.getElementById('5-condition')
+    },
+    6:{
+        icon: document.getElementById('6-icon'),
+        temp: document.getElementById('6-temp'),
+        feelsLike: document.getElementById('6-feelslike'),
+        condition:document.getElementById('6-condition')
+    },
+    7:{
+        icon: document.getElementById('7-icon'),
+        temp: document.getElementById('7-temp'),
+        feelsLike: document.getElementById('7-feelslike'),
+        condition:document.getElementById('7-condition')
+    },
+
 }
+
+
 
 export const indexDomItems = {
     loader: document.querySelector('#loader')
